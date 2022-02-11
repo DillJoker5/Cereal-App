0235338d0533c708ecac543d9bd98e0cc629b22a
Dylan Chirigotis <dylan.chirigotis@mail.wlc.edu>
2022-02-11 13:20:17 -0600

    Second commit

---
+++ cereal-app/src/components/AppNav.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app-bar color='light-blue accent-4' dense app dark>
+        <v-img max-width='40' src='@/assets/cerealLogo.png'> 
+        </v-img>
+        <v-app-bar-title>Cereal with Milk</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <v-btn depressed>Flavors</v-btn>
+        <v-btn depressed>Details</v-btn>
+        <v-btn depressed>Consumption Instructions</v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        name: 'AppNavigation',
+    };
+</script>
+
+<style scoped>
+
+</style>
